43104176e8419d41bf0ac2c290cab7f73f33956e
Lakshmi6-ui <lakshmi612255@gmail.com>
2026-02-12 12:28:19 +0530

    commpleted

--- array.java
+++ array.java
@@ -64,3 +64,31 @@ class arraymin{
     System.out.println(numbers[0][0]);
   }
 }
+
+//initalisizing the array
+class demo{
+  public static void main(String[] args){
+   // String[] playlists={"song1","song2","song3"};
+   // System.out.println(playlists[0]);
+    String[] playlists1=new String[3];
+    playlists1[0]="song1";
+    System.out.println(playlists1[0]);
+    playlists1[1]="song2";
+    System.out.println(playlists1[1]);
+    }
+}
+
+//find the max of array
+class arraymax{
+  public static void main(String[] args){
+    int[] array={10,30,60,80};
+    int max=0;
+    for(int i=0;i<array.length;i++)
+    {
+      if(array[i]>max){
+        max=array[i];
+      }
+    }
+    System.out.println("Max:"+max);
+  }
+}
